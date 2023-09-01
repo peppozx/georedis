a4744dd755b4ac5892c5e7d84fe6d54b666b372a
Igor Mattar <mctubegui@gmail.com>
2023-08-31 23:22:40 -0300

    mqtt client setup + docker

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import * as teste from './service/locationService';
+import mqttClient from './mqtt/mqtt.client';
+
 teste
+mqttClient
+
+mqttClient.subscribe('update_vehicle_location', (err: any) => {
+  if (!err) {
+    console.log('subscribed to topic');
+  }
+  mqttClient.on('message', (topic: any, message: any) => {
+    console.log(`Received message on topic ${topic}: ${message}`);
+  });
+  
+  mqttClient.publish('update_vehicle_location', JSON.stringify({ id: 'vehicle1', latitude: 1, longitude: 2 }), { qos: 0 }, () => {
+    console.log('Published message to topic: update_vehicle_location');
+  });
+});
